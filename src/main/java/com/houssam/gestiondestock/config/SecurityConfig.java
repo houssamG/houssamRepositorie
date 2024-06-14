3244720c6e0e9924e84dae6c503197c93d6c52c1
HoussamGHALIB <ghalib.houssam88@gmail.com>
2024-06-14 13:33:21 +0100

    commit change

--- src/main/java/com/houssam/gestiondestock/config/SecurityConfig.java
+++ src/main/java/com/houssam/gestiondestock/config/SecurityConfig.java
@@ -1,49 +1,64 @@
 package com.houssam.gestiondestock.config;
 
 
+import com.houssam.gestiondestock.model.Utilisateur;
+import com.houssam.gestiondestock.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+import java.util.ArrayList;
+import java.util.Collection;
 
+@Configuration
+public class SecurityConfig implements UserDetailsService{
+
+    @Autowired
+    UtilisateurRepository utilisateurRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 // toutes les requetes ont besoin d'etre autentifié
-        httpSecurity.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+        httpSecurity.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/v3/api-docs").permitAll().
+                requestMatchers("/gestiondestock/v1/**").authenticated());
 // demander a spring d'afficher un formulaire d'athentification pour toutes les requetes
         httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        //httpSecurity.csrf((csrf) -> csrf.ignoringRequestMatchers("/create"));
         return httpSecurity.build();
     }
 
-    @Bean
-    void authenticationManagerBuilder(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return null;
-            }
-        });
-    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails userDetails = User.withDefaultPasswordEncoder().username("houssam").password("houssam").build();
+//        return new InMemoryUserDetailsManager(userDetails);
+//    }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails userDetails = User.withDefaultPasswordEncoder().username("houssam").password("houssam").build();
-        return new InMemoryUserDetailsManager(userDetails);
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //UserDetails userDetails = User.withDefaultPasswordEncoder().username("houssam").password("houssam").build();
+        Utilisateur utilisateur = utilisateurRepository.findByMail(username);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        utilisateur.getRoles().forEach(role-> authorities.add(new SimpleGrantedAuthority(role.getNom())));
+        return new User(utilisateur.getMail(),utilisateur.getMotDepasse(),authorities);
     }
 }
